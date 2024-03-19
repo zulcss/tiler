a2641462da137934862f2bef45c4c22ae672819c
Charles Short <charles.short@windriver.com>
2024-03-19 17:32:15 +0000

    Add dev infrastructure
    See doc/source/developer.txt to setup a dev environment.
    
    Signed-off-by: Charles Short <charles.short@windriver.com>

---
+++ tools/run-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+     -i -t --privileged \
+     -v $(pwd):/usr/src \
+     -v /dev:/dev \
+     -v /run:/run \
+     -v /sys:/sys \
+     -v /var/www/html:/var/www/html \
+     tiler
---
+++ tools/run-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE=`date +%Y%m%d`
+LIBVIRT_STORAGE_PATH="/var/lib/libvirt/images/"
+
+
+virt-install --name tiler-test \
+    --osinfo debian12 \
+    --boot loader=/usr/share/ovmf/OVMF.fd \
+    --video virtio \
+    --disk path=$LIBVIRT_STORAGE_PATH/tiler-test-disk0.qcow2,format=qcow2,size=20,device=disk,bus=virtio,cache=none \
+    --cdrom ../live-config/pablo-live-$DATE.iso \
+    -noautoconsole \
+    --memory 3048 \
+    --vcpu 2
