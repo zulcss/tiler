cda84344d7044e2a76aeaa3bb1cf15a20e1bb417
Charles Short <charles.short@windriver.com>
2024-03-31 19:13:55 +0000

    Add filesystem module
    Add filesysem modules to format exxt4 and vfat.
    
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler/modules/installer/disk.py
+++ tiler/modules/installer/disk.py
@@ -5,6 +5,7 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import logging
+import os
 
 from tiler.modules.base import ModuleBase
 from tiler import utils
@@ -20,7 +21,7 @@ class Parted(ModuleBase):
 
         self.logging = logging.getLogger(__name__)
         self.device = self.config.params.disk
-        self.partitions = self.stage.partitions
+        self.slices = self.stage.slices
 
     def run(self):
         self.logging.info("Running parted.")
@@ -30,7 +31,7 @@ class Parted(ModuleBase):
             ["parted", self.device, "--script", "mklabel", "gpt"])
 
         self.logging.info("Creating parttiions.")
-        for index, part in enumerate(self.partitions, start=1):
+        for index, part in enumerate(self.slices, start=1):
             self.logging.info(f"Creating partition {index}: {part.name}")
             utils.run_command(
                 ["parted", "-a", "optimal", "-s", self.device, "--", "mkpart",
@@ -40,3 +41,50 @@ class Parted(ModuleBase):
                     utils.run_command(
                         ["parted", "-a", "optimal", "-s", self.device, "--",
                          "set", str(index), flag, "on"])
+
+
+class Filesystem(ModuleBase):
+    """Module that create filesystmes on parttions."""
+
+    def __init__(self, state, config, stage):
+        self.state = state
+        self.config = config
+        self.stage = stage
+
+        self.logging = logging.getLogger(__name__)
+        self.device = self.config.params.disk
+        self.filesystems = self.stage.filesystems
+
+    def run(self):
+        self.logging.info("Running filesystems.")
+
+        for index, part in enumerate(self.filesystems, start=1):
+            fs = self._get_partition_device(index, self.device)
+            if os.path.exists(fs):
+                self._mkfs(fs, part.fs, part.label, part.name)
+
+    def _mkfs(self, fs, fs_type, label, name):
+        self.logging.info(f"Formatting filesystems for {name}.")
+
+        if fs_type == "vfat":
+            # vfat is a special case
+            utils.run_command(
+                ["mkfs.vfat", "-F", "32", "-n", label, fs])
+        else:
+            utils.run_command(
+                ["mkfs", "-F", "-t", fs_type, "-L", label, fs])
+
+    def _get_partition_device(self, number, device):
+        """Get the parition device."""
+        suffix = "p"
+        # Check partition naming first: if used 'by-id'i naming convention
+        if "/disk/by-id" in device:
+            suffix = "-part"
+
+        # If the iamge device has a digit as the last character, the partition
+        # suffix is p<number> else it's just <number>
+        last = device[len(device) - 1]
+        if last >= "0" and last <= "9":
+            return "%s%s%d" % (device, suffix, number)
+        else:
+            return "%s%d" % (device, number)
