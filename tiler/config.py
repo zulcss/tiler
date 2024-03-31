0bc7933e6f789d1e8e8b45bd45c09ba03efa0655
Charles Short <charles.short@windriver.com>
2024-03-31 09:48:59 -0400

    Switch to use oamgeconf library
    Use omegaconf to load the yaml library.
    
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler/config.py
+++ tiler/config.py
@@ -3,21 +3,28 @@ Copyright (c) 2024 Wind River Systems, Inc.
 
 SPDX-License-Identifier: Apache-2.0
 """
+import os
 
+from omegaconf import OmegaConf
 import yaml
 
 from tiler import exceptions
 
-
 class Config(object):
+    """load the configuration file from the CLI."""
+
     def __init__(self, state):
         self.state = state
 
     def load_config(self):
-        """Load the autoinstaller configuration."""
-        with open(self.state.config, "r") as f:
-            try:
-                return yaml.safe_load(f)
-            except yaml.YAMLError:
-                raise exceptions.ConfigError(
-                    "Unable to parse configuration file.")
+        """Load the manifest.yaml"""
+        try:
+            with open(self.state.config, "r") as f:
+                try:
+                    return OmegaConf.create(yaml.safe_load(f))
+                except yaml.YAMLError as error:
+                    raise exceptions.ConfigError(
+                        f"{self.state.config} failed validateion: {error}.")
+        except OSError:
+            raise exceptions.ConfigError(
+                f"Configuration not found: {self.state.config}")
