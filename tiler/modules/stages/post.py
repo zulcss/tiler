224212743015eec0642e4847b62f1ecddf618f14
Charles Short <charles.short@windriver.com>
2024-04-01 00:02:22 +0000

    Add post-install stage
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler/modules/stages/post.py
+++ tiler/modules/stages/post.py
@@ -5,10 +5,12 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import logging
+import os
+
+from stevedore import driver
 
 from tiler.modules.base import ModuleBase
-
-LOG = logging.getLogger(__name__)
+from tiler.mount import umount
 
 
 class Post(ModuleBase):
@@ -16,5 +18,28 @@ class Post(ModuleBase):
         self.state = state
         self.config = config
 
+        self.logging = logging.getLogger(__name__)
+        self.rootfs = self.state.workspace.joinpath(
+            f"{self.config.name}/rootfs")
+
     def run(self):
-        pass
+        if self.config.stages.post:
+            for step in self.config.stages.post:
+                self.logging.info(step.name)
+                self.logging.info(f"Running {step.module}.")
+
+                try:
+                    mgr = driver.DriverManager(
+                        namespace="tiler.modules",
+                        name=step.module,
+                        invoke_on_load=True,
+                        invoke_args=(self.state, self.config, step.options)
+                    )
+                    mgr.driver.run()
+                except RuntimeError as e:
+                    raise Exception(
+                        f"Uable to load module: {step.module}: {e}"
+                    )
+        if os.path.ismount(self.rootfs):
+            self.logging.info("Cleaning up")
+            umount(self.rootfs)
