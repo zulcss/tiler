70f38ed48188486b371315a6198a584ae11167c7
Charles Short <charles.short@windriver.com>
2024-04-01 01:12:59 +0000

    Add ostree module
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- tiler/modules/installer/bootloader.py
+++ tiler/modules/installer/bootloader.py
@@ -5,6 +5,7 @@ SPDX-License-Identifier: Apache-2.0
 """
 
 import logging
+import shutil
 
 from tiler.modules.base import ModuleBase
 from tiler import utils
@@ -18,7 +19,6 @@ class Bootloader(ModuleBase):
         self.stage = stage
 
         self.logging = logging.getLogger(__name__)
-        self.device = self.config.params.disk
         self.kernel_args = self.stage.kernel_args
         self.rootfs = self.state.workspace.joinpath(
             f"{self.config.name}/rootfs")
@@ -44,3 +44,29 @@ class Bootloader(ModuleBase):
         utils.run_chroot_command(
             ["kernel-install", "add", kver, f"/boot/vmlinuz-{kver}"],
             self.rootfs, efi=self.rootfs)
+
+
+class BootloaderOstree(ModuleBase):
+    def __init__(self, state, config, stage):
+        self.state = state
+        self.config = config
+        self.stage = stage
+
+        self.logging = logging.getLogger(__name__)
+        self.rootfs = self.state.workspace.joinpath(
+            f"{self.config.name}/rootfs")
+
+    def run(self):
+        self.logging.info("Installing bootloader for ostree.")
+
+        for d in self.rootfs.glob("ostree/deploy/debian/deploy/*.0"):
+            repo_root = d
+        utils.run_chroot_command(
+            ["bootctl", "install"], repo_root, efi=self.rootfs)
+
+        self.logging.info("Configuring bootloader.")
+        shutil.copytree(self.rootfs.joinpath("boot/ostree"),
+                        self.rootfs.joinpath("efi/ostree"))
+        shutil.copy2(
+            self.rootfs.joinpath("boot/loader/entries/ostree-1-debian.conf"),
+            self.rootfs.joinpath("efi/loader/entries/ostree-0-1.conf"))
---
+++ tiler/modules/installer/ostree.py
@@ -0,0 +1,89 @@
+"""
+Copyright (c) 2024 Wind River Systems, Inc.
+
+SPDX-License-Identifier: Apache-2.0
+"""
+
+import logging
+
+from tiler.modules.base import ModuleBase
+from tiler.mount import mount
+from tiler import utils
+
+
+class OstreeInit(ModuleBase):
+    def __init__(self, state, config, stage):
+        self.state = state
+        self.config = config
+        self.stage = stage
+
+        self.logging = logging.getLogger(__name__)
+        self.device = self.config.params.disk
+        self.repo = self.config.params.repository
+        self.branch = self.config.params.branch
+        self.rootfs = self.state.workspace.joinpath(
+            f"{self.config.name}/rootfs")
+
+    def run(self):
+        self.logging.info("Unpacking source.")
+
+        self.logging.info(f"Mounting {self.device} on {self.rootfs}.")
+        mount(self.device, self.rootfs)
+
+        repo = self.rootfs.joinpath("ostree/repo")
+        self.logging.info(f"Creating {repo}")
+        if not repo.exists():
+            repo.mkdir(parents=True, exist_ok=True)
+        utils.run_command(
+            ["ostree", "init", "--repo", repo, "--mode", "bare"])
+
+
+class OstreePullLocal(ModuleBase):
+    def __init__(self, state, config, stage):
+        self.state = state
+        self.config = config
+        self.stage = stage
+
+        self.logging = logging.getLogger(__name__)
+        self.device = self.config.params.disk
+        self.repo = self.config.params.repository
+        self.branch = self.config.params.branch
+        self.rootfs = self.state.workspace.joinpath(
+            f"{self.config.name}/rootfs")
+
+    def run(self):
+        repo = self.rootfs.joinpath("ostree/repo")
+        self.logging.info(f"Pulling {self.branch} from {self.repo}")
+        utils.run_command(
+            ["ostree", "pull-local", "--repo", repo, self.repo, self.branch])
+
+
+class OstreeDeploy(ModuleBase):
+    def __init__(self, state, config, stage):
+        self.state = state
+        self.config = config
+        self.stage = stage
+
+        self.logging = logging.getLogger(__name__)
+        self.device = self.config.params.disk
+        self.repo = self.config.params.repository
+        self.branch = self.config.params.branch
+        self.kernel_args = self.stage.kernel_args
+        self.rootfs = self.state.workspace.joinpath(
+            f"{self.config.name}/rootfs")
+
+    def run(self):
+        self.logging.info(f"Configuring {self.device} for ostree.")
+        utils.run_command(
+            ["ostree", "admin", "init-fs", self.rootfs])
+        utils.run_command(
+            ["ostree", "admin", "os-init", "--sysroot", self.rootfs, "debian"])
+
+        self.logging.info(f"Deploying {self.branch}")
+        ostree_deploy = [
+            "ostree", "admin", "deploy",
+            "--sysroot", self.rootfs,
+            "--os", "debian", self.branch]
+        for arg in self.kernel_args:
+            ostree_deploy.append(f"--karg={arg}")
+        utils.run_command(ostree_deploy)
